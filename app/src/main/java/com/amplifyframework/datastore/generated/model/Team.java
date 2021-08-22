c41a3d3ac5dc22e3c5a230f1ed82dcf157986736
MKAbuMattar <mohammad.khaled@outlook.com>
2021-08-22 20:46:08 +0300

    cognito

--- app/src/main/java/com/amplifyframework/datastore/generated/model/Team.java
+++ app/src/main/java/com/amplifyframework/datastore/generated/model/Team.java
@@ -17,99 +17,89 @@ import com.amplifyframework.core.model.query.predicate.QueryField;
 
 import static com.amplifyframework.core.model.query.predicate.QueryField.field;
 
-/**
- * This is an auto generated class representing the Team type in your schema.
- */
+/** This is an auto generated class representing the Team type in your schema. */
 @SuppressWarnings("all")
 @ModelConfig(pluralName = "Teams")
 public final class Team implements Model {
   public static final QueryField ID = field("Team", "id");
   public static final QueryField NAME = field("Team", "name");
-  private final @ModelField(targetType = "ID", isRequired = true)
-  String id;
-  private final @ModelField(targetType = "String", isRequired = true)
-  String name;
-  private final @ModelField(targetType = "Task")
-  @HasMany(associatedWith = "team", type = Task.class)
-  List<Task> teamTasks = null;
-  private @ModelField(targetType = "AWSDateTime", isReadOnly = true)
-  Temporal.DateTime createdAt;
-  private @ModelField(targetType = "AWSDateTime", isReadOnly = true)
-  Temporal.DateTime updatedAt;
-
+  private final @ModelField(targetType="ID", isRequired = true) String id;
+  private final @ModelField(targetType="String", isRequired = true) String name;
+  private final @ModelField(targetType="Task") @HasMany(associatedWith = "team", type = Task.class) List<Task> teamTasks = null;
+  private @ModelField(targetType="AWSDateTime", isReadOnly = true) Temporal.DateTime createdAt;
+  private @ModelField(targetType="AWSDateTime", isReadOnly = true) Temporal.DateTime updatedAt;
   public String getId() {
-    return id;
+      return id;
   }
-
+  
   public String getName() {
-    return name;
+      return name;
   }
-
+  
   public List<Task> getTeamTasks() {
-    return teamTasks;
+      return teamTasks;
   }
-
+  
   public Temporal.DateTime getCreatedAt() {
-    return createdAt;
+      return createdAt;
   }
-
+  
   public Temporal.DateTime getUpdatedAt() {
-    return updatedAt;
+      return updatedAt;
   }
-
+  
   private Team(String id, String name) {
     this.id = id;
     this.name = name;
   }
-
+  
   @Override
-  public boolean equals(Object obj) {
-    if (this == obj) {
-      return true;
-    } else if (obj == null || getClass() != obj.getClass()) {
-      return false;
-    } else {
+   public boolean equals(Object obj) {
+      if (this == obj) {
+        return true;
+      } else if(obj == null || getClass() != obj.getClass()) {
+        return false;
+      } else {
       Team team = (Team) obj;
       return ObjectsCompat.equals(getId(), team.getId()) &&
-          ObjectsCompat.equals(getName(), team.getName()) &&
-          ObjectsCompat.equals(getCreatedAt(), team.getCreatedAt()) &&
-          ObjectsCompat.equals(getUpdatedAt(), team.getUpdatedAt());
-    }
+              ObjectsCompat.equals(getName(), team.getName()) &&
+              ObjectsCompat.equals(getCreatedAt(), team.getCreatedAt()) &&
+              ObjectsCompat.equals(getUpdatedAt(), team.getUpdatedAt());
+      }
   }
-
+  
   @Override
-  public int hashCode() {
+   public int hashCode() {
     return new StringBuilder()
-        .append(getId())
-        .append(getName())
-        .append(getCreatedAt())
-        .append(getUpdatedAt())
-        .toString()
-        .hashCode();
+      .append(getId())
+      .append(getName())
+      .append(getCreatedAt())
+      .append(getUpdatedAt())
+      .toString()
+      .hashCode();
   }
-
+  
   @Override
-  public String toString() {
+   public String toString() {
     return new StringBuilder()
-        .append("Team {")
-        .append("id=" + String.valueOf(getId()) + ", ")
-        .append("name=" + String.valueOf(getName()) + ", ")
-        .append("createdAt=" + String.valueOf(getCreatedAt()) + ", ")
-        .append("updatedAt=" + String.valueOf(getUpdatedAt()))
-        .append("}")
-        .toString();
+      .append("Team {")
+      .append("id=" + String.valueOf(getId()) + ", ")
+      .append("name=" + String.valueOf(getName()) + ", ")
+      .append("createdAt=" + String.valueOf(getCreatedAt()) + ", ")
+      .append("updatedAt=" + String.valueOf(getUpdatedAt()))
+      .append("}")
+      .toString();
   }
-
+  
   public static NameStep builder() {
-    return new Builder();
+      return new Builder();
   }
-
-  /**
+  
+  /** 
    * WARNING: This method should not be used to build an instance of this object for a CREATE mutation.
    * This is a convenience method to return an instance of the object with only its ID populated
    * to be used in the context of a parameter in a delete mutation or referencing a foreign key
    * in a relationship.
-   *
    * @param id the id of the existing item this instance will represent
    * @return an instance of this model with only ID populated
    * @throws IllegalArgumentException Checks that ID is in the proper format
@@ -119,87 +109,83 @@ public final class Team implements Model {
       UUID.fromString(id); // Check that ID is in the UUID format - if not an exception is thrown
     } catch (Exception exception) {
       throw new IllegalArgumentException(
-          "Model IDs must be unique in the format of UUID. This method is for creating instances " +
+              "Model IDs must be unique in the format of UUID. This method is for creating instances " +
               "of an existing object with only its ID field for sending as a mutation parameter. When " +
               "creating a new object, use the standard builder method and leave the ID field blank."
       );
     }
     return new Team(
-        id,
-        null
+      id,
+      null
     );
   }
-
+  
   public CopyOfBuilder copyOfBuilder() {
     return new CopyOfBuilder(id,
-        name);
+      name);
   }
-
   public interface NameStep {
     BuildStep name(String name);
   }
-
+  
 
   public interface BuildStep {
     Team build();
-
     BuildStep id(String id) throws IllegalArgumentException;
   }
-
+  
 
   public static class Builder implements NameStep, BuildStep {
     private String id;
     private String name;
-
     @Override
-    public Team build() {
-      String id = this.id != null ? this.id : UUID.randomUUID().toString();
-
-      return new Team(
+     public Team build() {
+        String id = this.id != null ? this.id : UUID.randomUUID().toString();
+        
+        return new Team(
           id,
           name);
     }
-
+    
     @Override
-    public BuildStep name(String name) {
-      Objects.requireNonNull(name);
-      this.name = name;
-      return this;
+     public BuildStep name(String name) {
+        Objects.requireNonNull(name);
+        this.name = name;
+        return this;
     }
-
-    /**
+    
+    /** 
      * WARNING: Do not set ID when creating a new object. Leave this blank and one will be auto generated for you.
      * This should only be set when referring to an already existing object.
-     *
      * @param id id
      * @return Current Builder instance, for fluent method chaining
      * @throws IllegalArgumentException Checks that ID is in the proper format
      */
     public BuildStep id(String id) throws IllegalArgumentException {
-      this.id = id;
-
-      try {
-        UUID.fromString(id); // Check that ID is in the UUID format - if not an exception is thrown
-      } catch (Exception exception) {
-        throw new IllegalArgumentException("Model IDs must be unique in the format of UUID.",
-            exception);
-      }
-
-      return this;
+        this.id = id;
+        
+        try {
+            UUID.fromString(id); // Check that ID is in the UUID format - if not an exception is thrown
+        } catch (Exception exception) {
+          throw new IllegalArgumentException("Model IDs must be unique in the format of UUID.",
+                    exception);
+        }
+        
+        return this;
     }
   }
-
+  
 
   public final class CopyOfBuilder extends Builder {
     private CopyOfBuilder(String id, String name) {
       super.id(id);
       super.name(name);
     }
-
+    
     @Override
-    public CopyOfBuilder name(String name) {
+     public CopyOfBuilder name(String name) {
       return (CopyOfBuilder) super.name(name);
     }
   }
-
+  
 }
